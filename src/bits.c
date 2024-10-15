bb0a9abc7c8c6528020b6b243154f837814125cf
Alok Swaminathan <swaminathanalok@gmail.com>
2024-10-15 18:16:50 -0400

    Set up data so it every bit is written in from lowest bit specific addr first

--- src/bits.c
+++ src/bits.c
@@ -1,11 +1,21 @@
 #include "types.h"
 
 static inline uint32_t reverse_bits(uint32_t data) {
-  unsigned int reversed = 0;
+  uint32_t reversed = 0;
   for (uint8_t i = 0; i < sizeof(data) * 8; i++){
     reversed |= ((data & (1 << i)) > 0) << ((63 - i) % 32);
   }
   return reversed;
+}
+
+static inline uint32_t reverse_bytes(uint32_t data) {
+  uint32_t rev_bytes = 0;
+  for (uint8_t i = 0; i < sizeof(data); i++) {
+    rev_bytes >>= 8;
+    uint32_t byte_mask = ((1 << 8) - 1) << (8*i);
+    rev_bytes |= reverse_bits(data & byte_mask) << (8*i);
+  }
+  return rev_bytes;
 }
 
 // Supports writes up to 16 bits, since that is the largest that will be needed
@@ -16,13 +26,15 @@ static inline uint32_t reverse_bits(uint32_t data) {
 uint16_t write_bits(byte *arr, uint16_t curr_bit, uint32_t data, byte bits){
   arr += curr_bit / 8;
   byte offset = curr_bit % 8;
-  byte existing_data = arr[0] & ((1 << offset) - 1);
+  byte existing_data = arr[0] & (((1 << offset) - 1) << (8 - offset));
+  existing_data >>= (8 - offset);
 
   data &= (1 << bits) - 1;
+  data += existing_data << bits;
   data = reverse_bits(data); 
   data >>= ((sizeof(data) * 8) - bits - offset);
 
-  data += existing_data;
+  data = reverse_bytes(data);
 
   ((uint32_t*)arr)[0] = data;
 
